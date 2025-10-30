974ec7c98ff06b06a23fc204555a7dcac5254a33
Borislav Atanasov <kalelzar@kalelzar.xyz>
2025-10-30 12:32:55 +0200

    Add a filter to the event list endpoint

--- src/models.zig
+++ src/models.zig
@@ -1,1 +1,6 @@
+const klib = @import("klib");
+
 pub const Cursor = @import("models/query.zig");
+pub const EventFilters = @import("models/event_filters.zig");
+
+pub const PaginatedEventsQuery = klib.meta.MergeStructs(Cursor, EventFilters);
